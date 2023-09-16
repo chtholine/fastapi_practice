d9a5fbcf7be23cac8b725e7cea9b799a867e729a
chtholine <oleg123qwe@gmail.com>
2023-09-16 17:48:04 +0300

    router

--- app/routers/router.py
+++ app/routers/router.py
@@ -1,13 +1,12 @@
 from fastapi import APIRouter
-from fastapi.openapi.models import Response
 
 router = APIRouter()
 
 
 @router.get("/")
 async def root():
-    return Response(
-        status_code=200,
-        detail='ok',
-        result='working'
-    )
+    return {
+        "status_code": 200,
+        "detail": "ok",
+        "result": "working"
+    }
