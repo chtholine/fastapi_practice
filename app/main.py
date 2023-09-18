bacf2a1bdcf2aa8d3c08f89f162ee95672b47897
chtholine <oleg123qwe@gmail.com>
2023-09-18 23:26:47 +0300

    CORS

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers.router import router
 
 app = FastAPI()
 
 app.include_router(router)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "0.0.0.0:8000",
+    "127.0.0.1:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
