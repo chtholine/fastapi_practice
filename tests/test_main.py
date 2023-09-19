b35ebeb6f8053d07397caa2743e6dab75991d66b
chtholine <oleg123qwe@gmail.com>
2023-09-19 21:36:12 +0300

    added fixture

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,3 @@
-from fastapi.testclient import TestClient
-
-from app.main import app
-
-client = TestClient(app)
-
-
-def test_read_main():
-    response = client.get("/")
+def test_healthcheck(test_client):
+    response = test_client.get("/")
     assert response.status_code == 200
