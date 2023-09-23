a8e722c776d9f3d2b66436f6ce5f93df42851cc6
chtholine <oleg123qwe@gmail.com>
2023-09-23 14:51:10 +0300

    env

--- app/core/config.py
+++ app/core/config.py
@@ -14,8 +14,8 @@ class Settings:
     POSTGRES_USER: str = os.getenv("POSTGRES_USER")
     POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
     POSTGRES_HOST: str = os.getenv("POSTGRES_HOST")
-    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)  # default postgres port is 5432
-    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT")  # default postgres port is 5432
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
     DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 
