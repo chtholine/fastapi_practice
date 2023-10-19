ee5231b0399cd2b2bab3f9bd73516bb4800f63cc
chtholine <oleg123qwe@gmail.com>
2023-10-19 21:00:43 +0300

    hid env variables

--- migrations/env.py
+++ migrations/env.py
@@ -6,6 +6,7 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
+from app.core.config import Settings
 from app.db.models import Base
 
 # this is the Alembic Config object, which provides
@@ -23,6 +24,7 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
 
+config.set_main_option("sqlalchemy.url", Settings.POSTGRES_URL)
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
